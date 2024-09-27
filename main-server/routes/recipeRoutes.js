097191c882d9050aa995d69f25a7328cbb55c1b2
rbantug <ronbantug22@gmail.com>
2024-09-28 01:17:47 +0800

    added recipeRoutes.js

---
+++ main-server/routes/recipeRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import makeExpressCallback from '../express-callback.js';
+import recipeController from '../controllers/index.js';
+
+const recipeRouter = express.Router()
+
+recipeRouter.get('/ingredients', makeExpressCallback(recipeController.getRecipeBasedOnIngredient))
+
+recipeRouter.get('/:name', makeExpressCallback(recipeController.getRecipeByName))
+
+recipeRouter
+    .route('/')
+    .get(makeExpressCallback(recipeController.getAllRecipes))
+
+recipeRouter
+    .route('/:id')
+    .get(makeExpressCallback(recipeController.getRecipeById))
+    .patch(makeExpressCallback(recipeController.patchUpdateIsFavorite))
+
+export default recipeRouter;
