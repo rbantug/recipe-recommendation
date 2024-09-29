5b80dfdc344e26fb171d9a77a22b4785ca2a8e7b
rbantug <ronbantug22@gmail.com>
2024-09-29 21:51:00 +0800

    updated entities/id.js - removed isValidId

--- main-server/entities/recipe/id.js
+++ main-server/entities/recipe/id.js
@@ -2,7 +2,7 @@ import { createId, isCuid } from "@paralleldrive/cuid2";
 
 const identity = Object.freeze({
     makeId: createId,
-    isValidId: isCuid 
+    // isValidId: isCuid worthless! Any string is a valid cuid.
 })
 
 export default identity;
