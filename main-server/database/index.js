034b9acb1c98b3414726431e50da7f806f4bae84
rbantug <ronbantug22@gmail.com>
2024-09-29 22:36:53 +0800

    updated database/index.js in order for Supertest to work with Mongo memory server

--- main-server/database/index.js
+++ main-server/database/index.js
@@ -1,30 +1,39 @@
 import { MongoClient } from "mongodb";
 
 import makeRecipeDb from "./recipeDB.js";
+import { connectDB } from "../../__test__/fixtures/mongoDB.js";
 
 ///////////////////
 // MONGODB
 ///////////////////
 
-const uri = "mongodb://superuser:superuser@localhost:6000/?authSource=admin&replicaSet=dbrs&readPreference=primary&directConnection=true&ssl=false"
+let recipesCollection;
+let recipesDB;
 
-const client = new MongoClient(uri)
-
-async function run() {
-    try {
-        await client.connect();
-        console.log("You are now connected to the DB")
-        const db = client.db("mainDB");
-        const recipesCollection = db.collection('recipes')
-        return recipesCollection;
-    } catch(e) {
-        console.error(e.message)
-        client.close();
+if(process.env.NODE_ENV !== "supertest") {
+    const uri = "mongodb://superuser:superuser@localhost:6000/?authSource=admin&replicaSet=dbrs&readPreference=primary&directConnection=true&ssl=false"
+    
+    const client = new MongoClient(uri)
+    
+    async function run() {
+        try {
+            await client.connect();
+            console.log("You are now connected to the DB")
+            const db = client.db("mainDB");
+            const recipesCollection = db.collection('recipes')
+            return recipesCollection;
+        } catch(e) {
+            console.error(e.message)
+            client.close();
+        }
     }
+    
+    const recipesCollection = await run();
+    
+    recipesDB = makeRecipeDb({ recipesCollection }) 
+} else {
+    recipesCollection = await connectDB()
+    recipesDB = makeRecipeDb({ recipesCollection })
 }
-
-const recipesCollection = await run();
-
-const recipesDB = makeRecipeDb({ recipesCollection }) 
 
 export default recipesDB;
