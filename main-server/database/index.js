79f15274d29da531d34fb380db632f2e9cf53f1f
rbantug <ronbantug22@gmail.com>
2024-10-04 00:34:43 +0800

    updated main-server/database/index.js - There are now 3 fake recipes

--- main-server/database/index.js
+++ main-server/database/index.js
@@ -2,6 +2,7 @@ import { MongoClient } from "mongodb";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 import makeRecipeDb from "./recipeDB.js";
+import makeFakeRecipe from "../../__test__/fixtures/recipes.js";
 
 ///////////////////
 // MONGODB
@@ -34,5 +35,13 @@ async function run() {
 const recipesCollection = await run();
 
 const recipesDB = makeRecipeDb({ recipesCollection })
+
+if( process.env.NODE_ENV === 'supertest' ) {
+    await recipesDB.insertManyRecipes([
+        makeFakeRecipe({ id: 'asweddfsefsdsdf322fefs11'}),
+        makeFakeRecipe({ id: 'e2hfgr20gtq6aii12llcoe9e'}),
+        makeFakeRecipe({ id: '1wn9gd525sufv1s5mt5lh62z'}),
+    ])
+}
 
 export default recipesDB;
