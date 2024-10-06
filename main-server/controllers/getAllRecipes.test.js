68662b3d6184bb371e94d31fab6f74c4da420bd7
rbantug <ronbantug22@gmail.com>
2024-10-06 22:49:14 +0800

    added new test - main-server/controllers/getAllRecipes.test.js

---
+++ main-server/controllers/getAllRecipes.test.js
@@ -0,0 +1,49 @@
+import { describe, expect, it, beforeAll } from "vitest";
+import request from 'supertest';
+
+import Server from '../server.js'
+import makeGetAllRecipes from "./getAllRecipes.js";
+import singleFakeRecipe from "../../__test__/fixtures/singleFakeRecipe.js";
+
+
+describe('GET /', () => {
+    const { app } = new Server()
+
+    describe('get all recipes', async () => {
+        const response = await request(app).get('/api/v1/recipes/')
+
+        it('should respond with status code 200', () => {
+            expect(response.statusCode).toBe(200)
+        })
+
+        it('should respond with status: "success" and a json object in a particular format', async () => {
+            const getAllRecipes = makeGetAllRecipes(() => [singleFakeRecipe])
+            const mockActual = await getAllRecipes()
+
+            expect(mockActual).toEqual(response.body)
+        })
+    })
+
+    describe('throws an error', async () => {
+        const getAllRecipes = makeGetAllRecipes(() => {
+            throw Error('test error')
+        })
+        const mockActual = await getAllRecipes()
+
+        it('should respond with status code 400', () => {
+            expect(mockActual.statusCode).toBe(400)
+        })
+
+        it('should respond with status: "fail" and a json object in a particular format', () => {
+            const mockResponse = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                statusCode: 400,
+                status: 'fail',
+                message: 'test error'
+            }
+            expect(mockActual).toEqual(mockResponse)
+        })
+    })
+})
