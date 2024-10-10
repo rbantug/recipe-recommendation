287a8d5a97d76687a9f68a9102696d90af628ef3
rbantug <ronbantug22@gmail.com>
2024-10-10 16:33:45 +0800

    build: added singleFakeUser.js

---
+++ __test__/fixtures/singleFakeUser.js
@@ -0,0 +1,12 @@
+export default {
+    id: 'anjccajfwwpmyz4o3qxfss0s',
+    email: 'Samson47@yahoo.com',
+    fullName: 'Mathias Schoen',
+    userName: 'Mathias_Schoen',
+    role: 'admin',
+    password: 'HoHZydUwqp',
+    favoriteRecipes: [ 'LKMNDQe3NBBqQDeKoOcT8jIq', 'o0TIRzkqSKl03SYILlmj9QkD' ],
+    createdAt: 2024-10-10T08:24:01.075Z,
+    lastModified: 2024-10-10T09:17:30.361Z,
+    active: true
+  }
