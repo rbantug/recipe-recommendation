fcffe1f490df436fa8e286e167873e89a52a164a
rbantug <ronbantug22@gmail.com>
2024-10-10 16:20:41 +0800

    build: added fake data for users

---
+++ __test__/fixtures/users.js
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+
+import identity from '../../utils/id';
+
+export default function makeFakeUser(overrides) {
+
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+
+    const user = {
+        id: identity.makeId(),
+        email: faker.internet.email(),
+        fullName: `${firstName} ${lastName}`,
+        userName: faker.internet.displayName({
+            firstName: firstName,
+            lastName: lastName
+        }), 
+        role: faker.helpers.arrayElement(['user', 'admin']),
+        password: faker.internet.password({
+            length: 10
+        }),
+        favoriteRecipes: Array.from({ length: Math.floor(Math.random() * 5) }, () => faker.string.alphanumeric({ length: 24 })),
+        createdAt: new Date(),
+        lastModified: faker.date.soon(),
+        active: faker.datatype.boolean(0.9)
+    }
+
+    return {
+        ...user,
+        ...overrides
+    }
+}
