bb9430e8eb2d4f089ed692350bed5b86e16bcd0c
rbantug <ronbantug22@gmail.com>
2024-10-17 16:26:38 +0800

    test: added a test that checks if it returns the correct recipe

--- main-server/use-cases/findRecipeById.test.js
+++ main-server/use-cases/findRecipeById.test.js
@@ -2,11 +2,14 @@ import { it, expect, describe, beforeAll } from "vitest";
 
 import makeFindRecipeById from "./findRecipeById.js";
 import identity from "../../utils/id.js"
+import makeFakeRecipe from "../../__test__/fixtures/recipes.js";
 
 let findRecipeById;
+let recipesDB;
 
 beforeAll(() => {
-    findRecipeById = makeFindRecipeById({ recipesDB: globalThis.recipesDB, isValid: identity.isValid })
+    recipesDB = globalThis.recipesDB
+    findRecipeById = makeFindRecipeById({ recipesDB, isValid: identity.isValid })
 })
 
 describe('findRecipeById', () => {
@@ -14,5 +17,19 @@ describe('findRecipeById', () => {
         const wrongId = '12asopduajdw'
         
         expect(findRecipeById(wrongId)).rejects.toThrow('This is not a valid recipe id')
+    })
+
+    it('should find the correct recipe', async () => {
+        const sampleData = [
+            makeFakeRecipe({ id: 'qurutw0s3qgfjhlwxnn6trl2' }),
+            makeFakeRecipe({ id: '63k4zxejioydowzhsh083puh' }),
+            makeFakeRecipe({ id: 't90llnii2ix0ho2o29sbjmkl' }),
+        ]
+
+        await recipesDB.insertManyRecipes(sampleData)
+
+        const getRecipe = await findRecipeById('63k4zxejioydowzhsh083puh')
+
+        expect(getRecipe).toEqual(sampleData[1])
     })
 })
