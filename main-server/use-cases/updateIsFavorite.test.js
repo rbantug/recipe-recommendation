eae3c31a82c0ccff8f901b019e8db7f27d1ffbe8
rbantug <ronbantug22@gmail.com>
2024-10-17 16:48:16 +0800

    test: added test that will check if the recipe was updated

--- main-server/use-cases/updateIsFavorite.test.js
+++ main-server/use-cases/updateIsFavorite.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, it, expect, describe } from "vitest";
+import { it, expect, describe, beforeEach } from "vitest";
 
 import identity from "../../utils/id.js";
 import makeUpdateIsFavorite from "./updateIsFavorite";
@@ -7,21 +7,42 @@ import makeFakeRecipe from "../../__test__/fixtures/recipes";
 let updateIsFavorite;
 
 const sampleData = [
-    makeFakeRecipe()
+    makeFakeRecipe({ id: 'wihb3j3its3rsqafyusfqm5o' })
 ]
 
-const insertArr = structuredClone(sampleData)
-
-beforeAll(async () => {
+beforeEach(async () => {
     updateIsFavorite = makeUpdateIsFavorite({ recipesDB: globalThis.recipesDB, isValid: identity.isValid })
 
-    await globalThis.recipesDB.insertManyRecipes(insertArr)
+    await globalThis.recipesDB.insertManyRecipes(sampleData)
 })
 
 describe('updateIsFavorite', () => {
     it('should accept valid object ids', () => {
-        const testObjectId = '12asopduajdw'
+        const testRecipeId = '12asopduajdw'
+
+        const sampleUserId = 'wxoa04yer8fwzgetrchjm3ie'
+
+        const updateRecipe = updateIsFavorite(sampleUserId, testRecipeId, sampleData[0].isFavorite)
         
-        expect(updateIsFavorite(testObjectId)).rejects.toThrow('This is not a valid recipe id')
+        expect(updateRecipe).rejects.toThrow('This is not a valid recipe id')
+    })
+
+    it('should update the recipe', async () => {
+        const sampleUserId = 'wxoa04yer8fwzgetrchjm3ie'
+
+        const getIsFavorite = sampleData[0].isFavorite
+
+        const updateRecipe = await updateIsFavorite(sampleUserId, 'wihb3j3its3rsqafyusfqm5o', getIsFavorite)
+
+        const date = new Date()
+        date.setSeconds(0,0)
+
+        const expectedResult = {
+            ...sampleData[0],
+            isFavorite: [...getIsFavorite, sampleUserId],
+            lastModified: date
+        }
+
+        expect(updateRecipe).toEqual(expectedResult)
     })
 })
