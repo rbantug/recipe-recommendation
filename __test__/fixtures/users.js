cbb4a130c6585de230c104e4d406782451646ccc
rbantug <ronbantug22@gmail.com>
2024-10-18 12:28:45 +0800

    refactor: email will now use the firstname and lastname

--- __test__/fixtures/users.js
+++ __test__/fixtures/users.js
@@ -6,27 +6,30 @@ export default function makeFakeUser(overrides) {
 
     const firstName = faker.person.firstName()
     const lastName = faker.person.lastName()
+    const password = faker.internet.password({
+        length: 10
+    })
 
     const user = {
         id: identity.makeId(),
-        email: faker.internet.email(),
+        email: faker.internet.email({
+            firstName, lastName
+        }),
         fullName: `${firstName} ${lastName}`,
         userName: faker.internet.displayName({
             firstName: firstName,
             lastName: lastName
         }), 
         role: faker.helpers.arrayElement(['user', 'admin']),
-        password: faker.internet.password({
-            length: 10
-        }),
-        passwordConfirm: this.password,
+        password,
+        passwordConfirm: password,
         passwordChangedAt: faker.date.soon(),
         passwordResetToken: faker.string.alphanumeric(10),
         passwordResetExpires: faker.date.soon(),
         favoriteRecipes: Array.from({ length: Math.floor(Math.random() * 5) }, () => faker.string.alphanumeric({ length: 24 })),
         createdAt: new Date(),
         lastModified: faker.date.soon(),
-        active: faker.datatype.boolean(0.9)
+        active: faker.datatype.boolean(1.0)
     }
 
     return {
