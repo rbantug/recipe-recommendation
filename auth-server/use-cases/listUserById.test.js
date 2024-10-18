9e63c7ff8842c2a0b3cea4f6ba6c9e14c1dad29e
rbantug <ronbantug22@gmail.com>
2024-10-18 12:29:58 +0800

    test: added test that will check if we get the correct user through userid

--- auth-server/use-cases/listUserById.test.js
+++ auth-server/use-cases/listUserById.test.js
@@ -2,11 +2,14 @@ import { it, expect, describe, beforeAll } from "vitest";
 
 import makeListUserById from "./listUserById.js";
 import identity from "../../utils/id.js";
+import makeFakeUser from "../../__test__/fixtures/users.js";
 
 let listUserById
+let usersDB
 
 beforeAll(() => {
-    listUserById = makeListUserById({ usersDB: globalThis.usersDB, isValid: identity.isValid })
+    usersDB = globalThis.usersDB
+    listUserById = makeListUserById({ usersDB, isValid: identity.isValid })
 })
 
 describe('listUserById', () => {
@@ -14,5 +17,19 @@ describe('listUserById', () => {
         const wrongId = 'aweawdauiwhd'
 
         expect(listUserById(wrongId)).rejects.toThrow('This is not a valid user id')
+    })
+
+    it('should return the correct user', async () => {
+        const sampleData = [
+            makeFakeUser({ id: '83vdugsqrt3pg2s0r9n144a1' }),
+            makeFakeUser({ id: '4vvxh35u5u8ihcgye5m5ku33' }),
+            makeFakeUser({ id: 'v267rr1m9z070ajg5jo4y9d6' })
+        ]
+
+        await Promise.all(sampleData.map(usersDB.insertUser))
+
+        const getUser = await listUserById('v267rr1m9z070ajg5jo4y9d6')
+
+        expect(getUser).toEqual(sampleData[2])
     })
 })
