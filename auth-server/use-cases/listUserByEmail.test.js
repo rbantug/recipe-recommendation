5926a76c0d92e488a8e5c4117896898779b833bb
rbantug <ronbantug22@gmail.com>
2024-10-18 12:18:40 +0800

    test: added listUserByEmail.test.js

---
+++ auth-server/use-cases/listUserByEmail.test.js
@@ -0,0 +1,35 @@
+import { it, expect, describe, beforeAll } from "vitest";
+import joi from 'joi'
+
+import makeListUserByEmail from "./listUserByEmail.js";
+import makeFakeUser from "../../__test__/fixtures/users.js";
+
+let listUserByEmail
+let usersDB
+
+beforeAll(() => {
+    usersDB = globalThis.usersDB
+    listUserByEmail = makeListUserByEmail({ usersDB, joi })
+})
+
+describe('listUserByEmail', () => {
+    it('should accept a valid email', () => {
+        const wrongEmail = 'awdawd.com@/bahkkj'
+
+        expect(listUserByEmail(wrongEmail)).rejects.toThrow('This is not a valid email')
+    })
+
+    it('should return the correct user', async () => {
+        const sampleData = [
+            makeFakeUser({ email: 'bubblybanana@gmail.com' }),
+            makeFakeUser({ email: 'deniedblueberry@gmail.com' }),
+            makeFakeUser({ email: 'unhingedpeach@gmail.com' }),
+        ]
+
+        await Promise.all(sampleData.map(usersDB.insertUser))
+
+        const getUser = await listUserByEmail('deniedblueberry@gmail.com')
+
+        expect(getUser).toEqual(sampleData[1])
+    })
+})
