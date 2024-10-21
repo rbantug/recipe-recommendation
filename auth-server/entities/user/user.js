aa82ec0afe4695e7a4874169cb11982ae7138b11
rbantug <ronbantug22@gmail.com>
2024-10-22 00:20:13 +0800

    build: user.js now has 2 schemas. What will be used will depend if the object is a for a new user or a password update

--- auth-server/entities/user/user.js
+++ auth-server/entities/user/user.js
@@ -5,46 +5,67 @@
  */
 
 function buildMakeUser({
-    joi,
-  }) {
-    return function makeUser({
-      id,
-      email,
-      userName,
-      fullName,
-      role,
-      password,
-      passwordConfirm,
-      passwordChangedAt,
-      passwordResetToken,
-      passwordResetExpires,
-      favoriteRecipes = [],
-      createdAt = Date.now(),
-      lastModified,
-      active = true,
-    } = {}) {
-      const schema = joi.object({
-        id: joi.string().length(24).required(),
-        email: joi.string().email().required(),
-        userName: joi.string().max(50).required(),
-        fullName: joi.string().max(50).required(),
-        role: joi.string().valid('user', 'admin').required(),
-        password: joi.string().min(6).required(),
-        passwordConfirm: joi.string().custom((value, helper) => {
-          if(value !== password) {
-            return helper.message('"passwordConfirm" is not the same with "password"')
-          }
-        }),
-        passwordChangedAt: joi.date(),
-        passwordResetToken: joi.string(),
-        passwordResetExpires: joi.date(),
-        favoriteRecipes: joi.array(),
-        createdAt: joi.date(),
-        active: joi.boolean(),
-        lastModified: joi.date(),
-      });
-  
-      const { error, value } = schema.validate({
+  joi
+}) {
+  return function makeUser({
+    id,
+    email,
+    userName,
+    fullName,
+    role,
+    password,
+    passwordConfirm,
+    passwordChangedAt,
+    passwordResetToken,
+    passwordResetExpires,
+    favoriteRecipes = [],
+    createdAt = Date.now(),
+    lastModified,
+    active = true,
+    type
+  } = {}) {
+    const newUserSchema = joi.object({
+      id: joi.string().length(24).required(),
+      email: joi.string().email().required(),
+      userName: joi.string().max(50).required(),
+      fullName: joi.string().max(50).required(),
+      role: joi.string().valid('user', 'admin').required(),
+      password: joi.string().min(6).required(),
+      passwordConfirm: joi.string().custom((value, helper) => {
+        if (value !== password) {
+          return helper.message('"passwordConfirm" is not the same with "password"')
+        }
+      }),
+      passwordChangedAt: joi.date(),
+      passwordResetToken: joi.string(),
+      passwordResetExpires: joi.date(),
+      favoriteRecipes: joi.array(),
+      createdAt: joi.date(),
+      active: joi.boolean(),
+      lastModified: joi.date(),
+      type: 'newUser'
+    });
+
+    const updatePasswordSchema = joi.object({
+      password: joi.string().min(6).required(),
+      passwordConfirm: joi.string().custom((value, helper) => {
+        if (value !== password) {
+          return helper.message('"passwordConfirm" is not the same with "password"')
+        }
+      }),
+      type: 'updatePassword'
+    })
+
+    const conditionalSchema = joi.any().when('.type', {
+      switch: [
+        { is: 'newUser', then: newUserSchema },
+        { is: 'updatePassword', then: updatePasswordSchema }
+      ]
+    })  
+
+    if (type === 'newUser') {
+
+      const { error, value } = conditionalSchema.validate({
         id,
         email,
         userName,
@@ -59,12 +80,26 @@ function buildMakeUser({
         createdAt,
         active,
         lastModified
-      }, { convert: false });
+      }, { convert: false })
   
       if (error) {
         throw new Error(error);
       }
-  
+    }
+
+      if (type === 'updatePassword') {
+
+        const { error, value } = conditionalSchema.validate({
+          password,
+          passwordConfirm,
+        }, { convert: false })
+    
+        if (error) {
+          throw new Error(error);
+        }
+        
+      }
+
       return Object.freeze({
         getId: () => id,
         getEmail: () => email,
@@ -79,10 +114,10 @@ function buildMakeUser({
         getFavoriteRecipes: () => favoriteRecipes,
         getCreatedAt: () => createdAt,
         getLastModified: () => lastModified,
-        getActive: () => active
+        getActive: () => active,
+        getType: () => type
       });
     };
   }
-  
-  export default buildMakeUser
-  
+
+export default buildMakeUser
