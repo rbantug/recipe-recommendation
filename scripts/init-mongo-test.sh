cb2a488953874721597a89051b88b6eeb9cf0734
rbantug <ronbantug22@gmail.com>
2024-10-23 23:37:04 +0800

    build: automatically create a MongoDB container during testing

---
+++ scripts/init-mongo-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+docker-compose -f docker-compose-mongo-test/docker-compose.yml up mongo-test -d
+
+echo "setting up mongo container"
+
+COUNTER=0
+until docker exec mongo-test bash -c mongosh --eval "printjson(db.serverStatus())"
+do
+  sleep 1
+  COUNTER=$((COUNTER+1))
+  if [[ ${COUNTER} -eq 30 ]]; then
+    echo "MongoDB did not initialize within 30 seconds, exiting"
+    exit 2
+  fi
+  echo "Waiting for MongoDB to initialize... ${COUNTER}/30"
+done
+
+echo "running unit test"
+
+vitest run --exclude "./main-server/controllers/*"
+
+echo "tearing down all containers"
+docker-compose -f docker-compose-mongo-test/docker-compose.yml down -v --remove-orphans
