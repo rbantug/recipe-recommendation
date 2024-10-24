69f8efb6636e2f4cf364358c102866a2b0f34c73
rbantug <ronbantug22@gmail.com>
2024-10-24 23:21:46 +0800

    build: updated auth-server/use-cases/index.js so that I can now move on to controllers

--- auth-server/use-cases/index.js
+++ auth-server/use-cases/index.js
@@ -1,15 +1,31 @@
+import joi from 'joi'
+
+import makeAddUser from "./addUser.js";
 import makeListUsers from "./listUsers.js";
 import makeListUserById from "./listUserById.js";
+import makeListUserByEmail from "./listUserByEmail.js"
+import makeSignToken from './signToken.js';
+import makeUpdatePassword from './updatePassword.js'
 
 import { usersDB } from "../database/userDB.js"
 import identity from "../../utils/id.js";
+import passwordEncrypt from '../../utils/passwordEncryption.js'
+import token from '../../utils/token.js'
 
+const addUser = makeAddUser({ usersDB, encrypt: passwordEncrypt.encrypt })
 const listUsers = makeListUsers({ usersDB })
 const listUserById = makeListUserById({ usersDB, isValid: identity.isValid })
+const listUserByEmail = makeListUserByEmail({ usersDB, joi })
+const signToken = makeSignToken({ createToken: token.sign })
+const updatePassword = makeUpdatePassword({ usersDB, encrypt: passwordEncrypt.encrypt })
 
 const userService = Object.freeze({
+    addUser,
     listUsers,
-    listUserById
+    listUserById,
+    listUserByEmail,
+    signToken,
+    updatePassword
 })
 
 export default userService;
