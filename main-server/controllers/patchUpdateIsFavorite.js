cca4acf2b3052b2933f3645028f9d066726d886a
rbantug <ronbantug22@gmail.com>
2024-10-30 21:49:06 +0800

    build: added main-server/controllers/patchUpdateIsFavorite.js

--- main-server/controllers/patchUpdateIsFavorite.js
+++ main-server/controllers/patchUpdateIsFavorite.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export default function makePatchUpdateIsFavorite(updateIsFavorite) {
     return async function patchUpdateIsFavorite(httpRequest) {
         const headers = {
@@ -5,9 +7,27 @@ export default function makePatchUpdateIsFavorite(updateIsFavorite) {
         }
 
         try {
-            const { ingredients, ...body } = httpRequest.body
+            const recipeId = httpRequest.params.id
 
-            const data = await updateIsFavorite(ingredients)
+            // the main server will send a recipeId
+            /*  the auth server will send an acknowledgement and the userId to the main server
+
+            something like this:
+
+            {
+                isLoggedIn: true,
+                userId: 'dw2asxasdd'
+            }
+
+            */
+            const {data: userData} = await axios({
+                method: 'get',
+                url: 'http://localhost:7000/auth'
+            })
+
+            if(!userData.isLoggedIn) throw new Error('User is not authenticated')
+
+            const data = await updateIsFavorite(userData.userId, recipeId)
 
             return {
                 headers,
