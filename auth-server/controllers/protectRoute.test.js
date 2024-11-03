c8f310043037d5521e38dd450a28ae061684722c
rbantug <ronbantug22@gmail.com>
2024-11-03 23:34:01 +0800

    test: added auth-server/controllers/protectRoute.test.js

---
+++ auth-server/controllers/protectRoute.test.js
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+import request from "supertest"
+
+import Server from "../../server";
+
+describe('protect middleware', () => {
+    const { app } = new Server
+
+    describe('given an invalid token', async () => {
+        // We will use the endpoint for updating the recipe isFavorite property
+        const response = await request(app)
+            .patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
+
+        it('should return a status code of 400', () => {
+            expect(response.statusCode).toBe(400)
+        })
+
+        it('should respond with status: "fail" and a json object in a particular format', () => {
+            const mockResponse = {
+                headers: { 'Content-Type': 'application/json' },
+                statusCode: 400,
+                status: 'fail',
+                message: 'The user is not logged in'
+            }
+
+            expect(response.body).toEqual(mockResponse)
+        })
+    })
+})
