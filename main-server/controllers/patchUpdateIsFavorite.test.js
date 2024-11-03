4bd702f0cb5e627917579f0ff5e975c07fbe02aa
rbantug <ronbantug22@gmail.com>
2024-11-03 15:57:33 +0800

    test: updated main-server/controllers/patchUpdateIsFavorite.test.js - removed nock - added bearer token in each supertest request - removed test that is related to the fetch request, which uses nock

--- main-server/controllers/patchUpdateIsFavorite.test.js
+++ main-server/controllers/patchUpdateIsFavorite.test.js
@@ -1,8 +1,15 @@
 import { describe, expect, it } from "vitest";
 import request from 'supertest';
-import nock from 'nock'
 
-import Server from '../server.js'
+import Server from '../../server.js'
+import token from "../../utils/token.js";
+
+process.env.JWT_SECRET = 'papaya'
+process.env.JWT_EXPIRES_IN = 600000
+
+const userId = 'ttvwv2pikps7f6ihm38cc3yw'
+
+const testToken = await token.signToken({ userId })
 
 const sampleData = {
   "id": "f9smrwidoda4rlswde7tjkwm",
@@ -21,31 +28,13 @@ const sampleData = {
   ]
 }
 
-function runNock(response) {
-  nock('http://localhost:7000')
-  .get('/auth')
-  .reply(200, response)
-}
-
-const mockValidResponse = {
-  isLoggedIn: true,
-  userId: 'nf28btxngkrvn8q0gogpnb86'
-}
-
-const mockInvalidResponse = {
-  isLoggedIn: false,
-  userId: 'nf28btxngkrvn8q0gogpnb86'
-} 
-
-runNock(mockValidResponse)
-runNock(mockValidResponse)
-runNock(mockInvalidResponse)
-
 describe('PATCH /:id', () => {
   const { app } = new Server
 
   describe('given a valid recipeId', async () => {
     const response = await request(app).patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
+      .set('Accept-Language', 'en')
+      .set('authorization', [`Bearer ${testToken}`])
 
     it('should respond with status 200', async () => {
       expect(response.statusCode).toBe(200)
@@ -56,7 +45,7 @@ describe('PATCH /:id', () => {
       date.setSeconds(0, 0)
 
       const copySampleData = structuredClone(sampleData)
-      copySampleData.isFavorite[1] = mockValidResponse.userId
+      copySampleData.isFavorite[1] = userId
       copySampleData.lastModified = date.toISOString()
 
       const mockResult = {
@@ -72,6 +61,8 @@ describe('PATCH /:id', () => {
 
   describe('given an invalid recipeId', async () => {
     const response = await request(app).patch('/api/v1/recipes/xxzjc67224x5ikm4adpb9s8w')
+    .set('Accept-Language', 'en')
+    .set('authorization', [`Bearer ${testToken}`])
 
     it('should respond with status 400', () => {
       expect(response.statusCode).toBe(400)
@@ -83,25 +74,6 @@ describe('PATCH /:id', () => {
         statusCode: 400,
         status: 'fail',
         message: 'The recipe does not exist'
-      }
-
-      expect(response.body).toEqual(mockResult)
-    })
-  })
-
-  describe('given an invalid user from fetched data', async () => {
-    const response = await request(app).patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
-
-    it('should respond with status code 400', () => {
-      expect(response.statusCode).toBe(400)
-    })
-
-    it('should respond with status: "fail" and a json object in a particular format', () => {
-      const mockResult = {
-        headers: { 'Content-Type': 'application/json' },
-        statusCode: 400,
-        status: 'fail',
-        message: 'User is not authenticated'
       }
 
       expect(response.body).toEqual(mockResult)
