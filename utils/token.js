28a80011c65495f2c396f08f1574676a9388b4ef
rbantug <ronbantug22@gmail.com>
2024-11-03 16:00:27 +0800

    refactor: turned asynchronous callback functions into async promise functions

--- utils/token.js
+++ utils/token.js
@@ -1,17 +1,50 @@
 // TODO: Try PASETO and JWT
 
 import jwt from 'jsonwebtoken'
+import { promisify } from 'util'
+
+/**
+ * A function that creates a token.
+ * @param {string} userId
+ * @returns {string}
+ */
+async function signToken(userId) {
+    try {
+        const signPromise = promisify(jwt.sign)
+
+        const newToken = await signPromise(
+            { userId },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        )
+
+        return newToken
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+/**
+ * A function that verifies the token and extracts the data that was encrypted in it
+ * @param {string} token - a token that was 
+ * @returns {*} the data encrypted in the token. It should contain the user ID
+ */
+async function verifyToken(token) {
+    try {
+        const verifyPromise = promisify(jwt.verify)
+
+        const decodedData = await verifyPromise(token, process.env.JWT_SECRET)
+
+        return decodedData
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const token = Object.freeze({
-    sign: (userId) => jwt.sign(userId, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    }),
-    verify: (token) => {
-        return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if(err) throw new Error(err)
-            return decoded
-        })
-    }
+    signToken,
+    verifyToken
 })
 
 export default token
