4ed59df5bc9746786e1a49fcb10f05e577f002a4
rbantug <ronbantug22@gmail.com>
2024-11-07 02:48:24 +0800

    test: updated tests to use global error handling middleware

--- auth-server/controllers/middlewares/protectRoute.test.js
+++ auth-server/controllers/middlewares/protectRoute.test.js
@@ -6,7 +6,34 @@ import Server from "../../../server";
 describe('protect middleware', () => {
     const { app } = new Server
 
-    describe('given an invalid token', async () => {
+    describe('given an invalid token while NODE_ENV = "development"', async () => {
+        process.env.NODE_ENV = 'development'
+
+        // We will use the endpoint for updating the recipe isFavorite property
+        const response = await request(app)
+            .patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
+
+        it('should return a status code of 401', () => {
+            expect(response.statusCode).toBe(401)
+        })
+
+        it('should respond with status: "fail" and a json object in a particular format', () => {
+            const mockResponse = {
+                headers: { 'Content-Type': 'application/json' },
+                statusCode: 401,
+                status: 'fail',
+                message: 'The user is not logged in'
+            }
+
+            delete response.body.stack
+
+            expect(response.body).toEqual(mockResponse)
+        })
+    })
+
+    describe('given an invalid token while NODE_ENV = "production"', async () => {
+        process.env.NODE_ENV = 'production'
+
         // We will use the endpoint for updating the recipe isFavorite property
         const response = await request(app)
             .patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
--- auth-server/controllers/middlewares/restrictedTo.test.js
+++ auth-server/controllers/middlewares/restrictedTo.test.js
@@ -15,12 +15,13 @@ const testToken = await token.signToken({ userId })
 describe('restrictedTo middleware', () => {
     const { app } = new Server
 
-    describe('given a user with an "admin" role is trying to access a endpoint only for "user" roles', async () => {
+    describe('given a user with an "admin" role is trying to access a endpoint only for "user" roles AND NODE_ENV = "development"', async () => {
+        process.env.NODE_ENV = 'development'
+
         // With patchUpdateIsFavorite, only users with the role of 'users' can access the endpoint
         const response = await request(app)
             .patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
             .set('authorization', [`Bearer ${testToken}`])
-            //console.log(response.body)
 
         it('should respond with status 403', () => {
             expect(response.statusCode).toBe(403)
@@ -32,9 +33,35 @@ describe('restrictedTo middleware', () => {
                 statusCode: 403,
                 status: 'fail',
                 message: 'You are not authorized to perform this action'
-              }
+            }
 
-              expect(response.body).toEqual(mockResult)
+            delete response.body.stack
+
+            expect(response.body).toEqual(mockResult)
+        })
+    })
+
+    describe('given a user with an "admin" role is trying to access a endpoint only for "user" roles AND NODE_ENV = "production"', async () => {
+        process.env.NODE_ENV = 'production'
+
+        // With patchUpdateIsFavorite, only users with the role of 'users' can access the endpoint
+        const response = await request(app)
+            .patch('/api/v1/recipes/f9smrwidoda4rlswde7tjkwm')
+            .set('authorization', [`Bearer ${testToken}`])
+
+        it('should respond with status 403', () => {
+            expect(response.statusCode).toBe(403)
+        })
+
+        it('should respond with status: "fail" and a json object in a particular format', () => {
+            const mockResult = {
+                headers: { 'Content-Type': 'application/json' },
+                statusCode: 403,
+                status: 'fail',
+                message: 'You are not authorized to perform this action'
+            }
+
+            expect(response.body).toEqual(mockResult)
         })
     })
 })
