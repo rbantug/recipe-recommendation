3c746f2ad0b2e843336d9a44a0868d38ac3bc9d4
rbantug <ronbantug22@gmail.com>
2024-11-08 01:43:27 +0800

    build: isOperational now relies on the status code for its value

--- utils/AppError.js
+++ utils/AppError.js
@@ -1,9 +1,10 @@
 export default class AppError extends Error {
-    constructor(message, statusCode) {
+    constructor(message, statusCode, headers) {
         super(message)
         this.statusCode = statusCode
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
-        this.isOperational = 'true'
+        this.isOperational = `${statusCode}`.startsWith('4') ? true : false
+        this.headers = headers
         Error.captureStackTrace(this, this.constructor)
     }
 }
