336e1984b85c36ccbdbe1c867529449b3a7db795
rbantug <ronbantug22@gmail.com>
2024-11-09 00:35:35 +0800

    build: Added login and signup in auth-server/controllers/index.js

--- auth-server/controllers/index.js
+++ auth-server/controllers/index.js
@@ -1,12 +1,17 @@
 import userService from "../use-cases/index.js"
-import makeProtectRoute from "./protectRoute.js"
+import makeSignup from "./signup.js"
+import makeLogin from "./login.js"
 
 import token from "../../utils/token.js"
+import passwordEncrypt from "../../utils/passwordEncryption.js"
+import AppError from "../../utils/AppError.js"
 
-const protectRoute = makeProtectRoute(userService.listUserById, token.verifyToken)
+const signup = makeSignup({ addUser: userService.addUser, signToken: token.signToken, passwordEncrypt: passwordEncrypt.encrypt, AppError })
+const login = makeLogin({ listUserByEmail: userService.listUserByEmail, passwordCompare: passwordEncrypt.compare, signToken: token.signToken, AppError })
 
 const userController = Object.freeze({
-    protectRoute
+    signup,
+    login
 })
 
 export default userController
