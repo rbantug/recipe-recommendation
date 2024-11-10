baacb1a768bdcd868d060c45f940697a019f0cd3
rbantug <ronbantug22@gmail.com>
2024-11-10 11:30:53 +0800

    refactor: changed from arrow function to function declaration

--- utils/passwordEncryption.js
+++ utils/passwordEncryption.js
@@ -1,10 +1,19 @@
 import bcryptjs from "bcryptjs";
 
-const { hash, compare } = bcryptjs
+//const { hash, compare } = bcryptjs
+
+async function encryptPassword(password) {
+    return bcryptjs.hash(password, 12)
+}
+
+async function comparePassword(loginPassword, databasePassword) {
+    const checkPassword = bcryptjs.compare(loginPassword, databasePassword)
+    return checkPassword
+}
 
 const passwordEncrypt = Object.freeze({
-    encrypt: async (password) => await hash(password, 12),
-    compare: async (loginPassword, databasePassword) => await compare(loginPassword, databasePassword)
+    encryptPassword,
+    comparePassword
 })
 
 export default passwordEncrypt
