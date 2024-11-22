5382079359be434d2ccd2686582e5272e777ad3f
rbantug <ronbantug22@gmail.com>
2024-11-22 18:19:21 +0800

    build: added "/forgotPassword" route

--- auth-server/routes/userRoutes.js
+++ auth-server/routes/userRoutes.js
@@ -8,6 +8,7 @@ const userRouter = express.Router()
 
 userRouter.post('/login', makeExpressCallback(userController.login, AppError))
 userRouter.post('/signup', makeExpressCallback(userController.signup, AppError))
+userRouter.patch('/forgotPassword', makeExpressCallback(userController.patchForgetPassword, AppError))
 
 
 export default userRouter
