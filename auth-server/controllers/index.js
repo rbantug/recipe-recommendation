5cb1b9c760ecd2b7620de3b03446745cc4386408
rbantug <ronbantug22@gmail.com>
2024-11-22 18:43:44 +0800

    build: added patchForgotPassword

--- auth-server/controllers/index.js
+++ auth-server/controllers/index.js
@@ -1,17 +1,21 @@
 import userService from "../use-cases/index.js"
 import makeSignup from "./signup.js"
 import makeLogin from "./login.js"
+import makePatchForgotPassword from "./patchForgotPassword.js"
 
 import token from "../../utils/token.js"
 import passwordEncrypt from "../../utils/passwordEncryption.js"
 import AppError from "../../utils/AppError.js"
+import Email from "../../utils/email.js"
 
 const signup = makeSignup({ addUser: userService.addUser, listUserByEmail: userService.listUserByEmail, signToken: token.signToken })
 const login = makeLogin({ listUserByEmail: userService.listUserByEmail, passwordCompare: passwordEncrypt.comparePassword, signToken: token.signToken, AppError })
+const patchForgetPassword = makePatchForgotPassword({ listUserByEmail: userService.listUserByEmail, editPasswordResetTokenAndExpires: userService.editPasswordResetTokenAndExpires, Email, AppError })
 
 const userController = Object.freeze({
     signup,
-    login
+    login,
+    patchForgetPassword
 })
 
 export default userController
