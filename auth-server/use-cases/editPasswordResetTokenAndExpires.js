b86b7a7bd6461894d2e70d71b9b73345a15616c2
rbantug <ronbantug22@gmail.com>
2024-11-22 18:44:51 +0800

    build: added new functionality that will reset passwordResetExpires and passwordResetToken to null if reset password email was not sent

--- auth-server/use-cases/editPasswordResetTokenAndExpires.js
+++ auth-server/use-cases/editPasswordResetTokenAndExpires.js
@@ -1,23 +1,31 @@
 import makeUser from "../entities/user";
 
 export default function makeEditPasswordResetTokenAndExpires({ usersDB, crypto }) {
-    return async function editPasswordResetTokenAndExpires({  userId }) {
-        const resetToken = crypto.randomBytes(32).toString('hex')
-        const passwordResetToken = crypto
-            .createHash('sha256')
-            .update(resetToken)
-            .digest('hex')
-        const passwordResetExpires = new Date(Date.now() + 600000)
-        passwordResetExpires.setSeconds(0,0)
+    return async function editPasswordResetTokenAndExpires({ userId, createToken }) {
+        let resetToken, passwordResetToken, passwordResetExpires
+
+        if(createToken) {
+            resetToken = crypto.randomBytes(32).toString('hex')
+            passwordResetToken = crypto
+                .createHash('sha256')
+                .update(resetToken)
+                .digest('hex')
+            passwordResetExpires = new Date(Date.now() + 600000)
+            passwordResetExpires.setSeconds(0,0)
+        } else {
+            resetToken = 'reset token and expiry date were reverted back to null'
+            passwordResetToken = null
+            passwordResetExpires = null
+        }
 
         const user = makeUser({ type: 'resetToken', passwordResetToken, passwordResetExpires })
 
-        const patchUser = await usersDB.updateUser(userId, {
+        const updatedUser = await usersDB.updateUser(userId, {
             passwordResetToken: user.getPasswordResetToken(),
             passwordResetExpires: user.getPasswordResetExpires(),
             type: user.getType()
         })
 
-        return patchUser
+        return { resetToken, updatedUser }
     }
 }
