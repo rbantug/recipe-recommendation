46a4e473f4729669e65ce07f37ddece3a7f6828b
rbantug <ronbantug22@gmail.com>
2024-11-27 01:17:18 +0800

    fix: added a check if the properties in "userInfo" are "password" and "passwordConfirm"

--- auth-server/use-cases/editPassword.js
+++ auth-server/use-cases/editPassword.js
@@ -2,12 +2,19 @@ import makeUser from "../entities/user/index.js";
 
 export default function makeEditPassword({ usersDB, encrypt }) {
     return async function editPassword({ userInfo, userId }) {
+        const getProps = Object.keys(userInfo)
+        const checkProps = getProps.every(x => x === 'password' || x === 'passwordConfirm')
+
+        if (!checkProps) {
+            throw new Error('You can only accept the "password" and "passwordConfirm"')
+        }
+
         const user = makeUser({ type: 'updatePassword', ...userInfo })
 
         const hashedPassword = await encrypt(user.getPassword())
 
         const date = new Date()
-        date.setSeconds(0,0)
+        date.setSeconds(0, 0)
 
         const patchPassword = await usersDB.updateUser(userId, {
             password: hashedPassword,
