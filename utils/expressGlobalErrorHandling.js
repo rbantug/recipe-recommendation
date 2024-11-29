2a5a626d58c424e98c5ed13a0f69a6678900fe7b
rbantug <ronbantug22@gmail.com>
2024-11-29 23:10:27 +0800

    build: made several changes to make global error handling more robust - errors during production will be filtered to check if they came from the controllers or use-cases/entities - some errors will have their messages changed to made them more user friendly. - programming errors thrown during production will send generic messages to users and will call console.error() for easier debugging

--- utils/expressGlobalErrorHandling.js
+++ utils/expressGlobalErrorHandling.js
@@ -1,10 +1,41 @@
+import AppError from "./AppError"
+
 export default function (err, req, res, next) {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, req, res)
     } else if (process.env.NODE_ENV === 'production') {
+
+        if (err.message === 'The recipe does not exist') {
+            err = handleNonexistingResource(err)
+        }
+
+        if (err.message === 'The user does not exist') {
+            err = handleNonexistingResource(err)
+        }
+        
+        if (err.message === 'This query is invalid') {
+            err = handleNonexistingResource(err)
+        }
+
+        if (err.message.startsWith('ValidationError')) {
+            err = handleJoiValidationError(err)
+        }
+
+        // These are programming errors. I need to keep the stack trace for debugging. The only way to test them is to deliberately add a bug in your code.
+
+        if (err.message === 'You can only change properties that are related to resetting the token') {
+            const newProps = handleErrorDB()
+            err = { ...err, ...newProps }
+            err.programmingError = true
+        }
+
+        if (err.message === 'You can\'t change these properties') {
+            const newProps = handleErrorDB()
+            err = { ...err, ...newProps }
+            err.programmingError = true
+        }
+
         sendErrorProd(err, req, res)
     }
 }
@@ -23,21 +54,31 @@ function sendErrorDev (err, req, res) {
 
 function sendErrorProd (err, req, res) {
     if (req.originalUrl.startsWith('/api')) {
-        if (err.isOperational) {
-            res.status(err.statusCode).json({
-                headers: err.headers,
-                status: err.status,
-                statusCode: err.statusCode,
-                message: err.message
-            })
-        } else {
-            console.error('This is a programming error', err)
-
-            res.status(500).json({
-                status: 'error',
-                message: 'Something went wrong'
-            })
+        if(err.programmingError) {
+            console.error(err)
         }
+
+        res.status(err.statusCode).json({
+            headers: err.headers,
+            status: err.status,
+            statusCode: err.statusCode,
+            message: err.message
+        })
     }
 }
 
+function handleNonexistingResource(err) {
+    return new AppError(err.message, 404)
+}
+
+function handleJoiValidationError(err) {
+    const errMsg = err.message.split(': ')
+    return new AppError(`Invalid input data: ${errMsg[1]}`, 400)
+}
+
+function handleErrorDB() {
+    return {
+        statusCode: 500,
+        message: 'Something went wrong'
+    }
+}
